9abb3e203c3417fcc3dccdfe8c19a8a27abd9ccb
Augusto <iadanza.augusto@gmailcom>
2026-09-18 11:42:20 -0300

    Fix: usar rutas relativas para las imagenes en GitHub Pages

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,7 +1,7 @@
-﻿export const heroDream = "/images/hero-dream.jpg";
-export const teddyBalloon = "/images/teddy-balloon.jpg";
-export const cloudStars = "/images/cloud-stars.jpg";
-export const floralCorner = "/images/floral-corner.jpg";
-export const babyFeet = "/images/baby-feet.jpg";
-export const moonStars = "/images/moon-stars.jpg";
-export const stork = "/images/stork.jpg";
+﻿export const heroDream = "./images/hero-dream.jpg";
+export const teddyBalloon = "./images/teddy-balloon.jpg";
+export const cloudStars = "./images/cloud-stars.jpg";
+export const floralCorner = "./images/floral-corner.jpg";
+export const babyFeet = "./images/baby-feet.jpg";
+export const moonStars = "./images/moon-stars.jpg";
+export const stork = "./images/stork.jpg";
